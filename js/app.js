37164be55e70d0bfcfb9adfea86427ad0973d147
Erick Godoy <erickgodoy725@gmail.com>
2022-10-04 15:09:57 -0500

    speed wind

--- js/app.js
+++ js/app.js
@@ -4,22 +4,20 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));      
     },
     displayWeather: function(data){
         const { name } = data;
-        const { icon,description } = data.weather;
+        const { icon,description } = data.weather[0];
         const { temp,humidity } = data.main;
         const { speed } = data.wind;
         console.log(name,icon,description,temp,humidity,speed); 
-        document.querySelector(".city").innerText = "Weather in" + name;
+        document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = 
-        temp + "°C";
-        document.querySelector(".humidity").innerText = humidity + "%";
-        document.querySelector(".Wind speed").innerText = 
-        temp + "km/h ";   
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + " %";
+        document.querySelector(".wind").innerText = "Wind: " + speed + "km/h ";   
         document.querySelector(".weather").classList.remove("loading");
     },
     search: function(){

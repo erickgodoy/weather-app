9b44aded6d1dd6bc4c9fc6914a53011bf31ef39a
Erick Godoy <erickgodoy725@gmail.com>
2022-10-04 19:44:34 -0500

    loading data

--- js/app.js
+++ js/app.js
@@ -4,17 +4,19 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey)
         .then((response) => response.json())
-        .then((data) => this.displayWeather(data));      
+        .then((data) => this.displayWeather(data))
+   
     },
     displayWeather: function(data){
         const { name } = data;
         const { icon,description } = data.weather[0];
         const { temp,humidity } = data.main;
         const { speed } = data.wind;
+        const { country } = data.sys;
         console.log(name,icon,description,temp,humidity,speed); 
-        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".city").innerText = name +" , " + country;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector(".description").innerText = description;
+        // document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + " %";
         document.querySelector(".wind").innerText = "Wind: " + speed + "km/h ";   

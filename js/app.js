1f0af3c1648205a4209cbd60a11ee83be5e6e021
Erick Godoy <erickgodoy725@gmail.com>
2022-10-05 15:53:46 -0500

    weather loading

--- js/app.js
+++ js/app.js
@@ -9,18 +9,18 @@ let weather = {
     },
     displayWeather: function(data){
         const { name } = data;
+        const { country } = data.sys;
         const { icon,description } = data.weather[0];
         const { temp,humidity } = data.main;
         const { speed } = data.wind;
-        const { country } = data.sys;
         console.log(name,icon,description,temp,humidity,speed); 
         document.querySelector(".city").innerText = name +" , " + country;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-        // document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + " %";
         document.querySelector(".wind").innerText = "Wind: " + speed + "km/h ";   
         document.querySelector(".weather").classList.remove("loading");
+        // document.querySelector(".side-right").style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+name+"')";
     },
     search: function(){
         this.fetchWeather(document.querySelector(".search-bar").value);
